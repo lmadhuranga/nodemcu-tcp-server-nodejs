75fcf05bec87f68c6507070b055be5fbdd91c0fc
lmadhuranga <lilan.maduranga@gmail.com>
2018-12-30 16:41:36 +0530

    update package json file

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-// // Server COde
-
-// const net = require('net');
-// const port = 8000;
-// const server = net.createServer((socket) => {
-//   // 'connection' listener
-// 	console.log('client connected');
-// 	socket.write('Welcome to the wonder server\r\n');
-
-// 	socket.on('end', () => {
-// 		console.log('client disconnected');
-// 		socket.end('Goodbye!\n');
-// 	});
-
-// 	socket.on('data', (data) => {
-// 		console.log('client data',data.toString());
-// 		setInterval(() => {
-// 			console.log('shit time out');
-// 			socket.write('fuck form server\r\n', new Date());
-// 		}, 30000);
-// 	});
-
-// });
-
-// server.on('error', (err) => {
-// 	console.log('err',err);
-//   throw err;
-// });
- 
-// server.listen(port, () => {
-//   console.log('server bound', port);
-// });
-
 // Clinet code
 const net = require('net');
 const client = net.createConnection(8000,'192.168.15.137', () => {
